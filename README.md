6366dd0ce12ba45f2d8d10b8f7b43d7d941b52ca
Ayush Rawat <ayushrawat3338@gmail.com>
2025-03-02 01:28:27 +0530

    ready set go!

---
+++ README.md
@@ -0,0 +1,34 @@
+# i18n
+
+# LibreTranslate Setup with Colima & Docker
+
+## Prerequisites
+- Install [Colima](https://github.com/abiosoft/colima)
+- Install [Docker](https://www.docker.com/)
+
+## Setup & Run
+
+### 1. Start Colima
+```sh
+colima start
+```
+
+### 2. Pull LibreTranslate Docker Image and Run it on $LT_PORT
+```sh
+git clone https://github.com/drsqrt/i18n.git
+cd i18n
+sudo chmod 755 run.sh
+./run.sh
+```
+
+### 3. Access LibreTranslate
+- Open [http://localhost:5050](http://localhost:5050) in your browser.
+
+### 4. Test API with cURL
+```sh
+curl -X POST "http://localhost:5050/translate" -H "Content-Type: application/json" \
+-d '{"q": "Hello", "source": "en", "target": "ja"}'
+```
+
+
+
---
+++ src/main/java/com/github/drsqrt/Translate.java
@@ -0,0 +1,56 @@
+package com.github.drsqrt;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.json.JSONObject;
+
+
+public class Translate {
+
+  /**
+   * Other end points to use :
+   * https://translate.flossboxin.org.in/translate, https://libretranslate.com/translate
+   */
+  @SuppressWarnings("JavadocLinkAsPlainText")
+  private static final String API_URL = "http://localhost:5050/translate";
+  private static final OkHttpClient client = new OkHttpClient();
+
+  public static void main(String[] args) {
+    String textToTranslate = "Hello, how are you?";
+
+    translateText(textToTranslate, "ja"); // Japanese
+    translateText(textToTranslate, "hi"); // Hindi
+    /*translateText(textToTranslate, "de");*/ // German
+  }
+
+  private static void translateText(String text, String targetLanguage) {
+    try {
+      JSONObject jsonBody = new JSONObject();
+      jsonBody.put("q", text);
+      jsonBody.put("source", "en");
+      jsonBody.put("target", targetLanguage);
+      jsonBody.put("format", "text");
+      RequestBody body = RequestBody.create(jsonBody.toString(),
+        MediaType.get("application/json; charset=utf-8"));
+      Request request = new Request.Builder()
+        .url(API_URL)
+        .post(body)
+        .addHeader("Content-Type", "application/json")
+        .build();
+
+      Response response = client.newCall(request).execute();
+      if (response.isSuccessful() && response.body() != null) {
+        String responseBody = response.body().string();
+        System.out.println("Translated (" + targetLanguage + "): " + responseBody);
+      } else {
+        System.out.println("Translation failed: " + response.code());
+      }
+      response.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
